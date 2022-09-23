a6ef8190f5318b4c7b06a920fc13813a26d25404
Samuel Ackon <ackons433@gmail.com>
2022-09-23 08:44:36 +0000

    Fix linter error

--- mylist.rb
+++ mylist.rb
@@ -1,11 +1,11 @@
 class MyList
-    include MyEnumerable
-    def initialize(*args)
-      @list = args.flatten
-    end
-  
-    def each(&block)
-      @list.each(&block)
-      self # return the original array
-    end
+  include MyEnumerable
+  def initialize(*args)
+    @list = args.flatten
   end
+
+  def each(&block)
+    @list.each(&block)
+    self # return the original array
+  end
+end
