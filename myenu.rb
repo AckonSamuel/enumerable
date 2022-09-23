92e4f2c7b521f34f27d50af1677b8e78c07776fe
Owusu-Desmond <desmond0406114009@gmail.com>
2022-09-23 08:39:00 +0000

    Create my list class

--- myenu.rb
+++ myenu.rb
@@ -1,15 +1,3 @@
-class MyList
-  include MyEnumerable
-  def initialize(*args)
-    @list = args.flatten
-  end
-
-  def each(&block)
-    @list.each(&block)
-    self # return the original array
-  end
-end
-
 module MyEnumerable
   def all?(&block)
     result = true
